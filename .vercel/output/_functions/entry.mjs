c201ec3bf4d12c9d7acdb2f02dcdac04312bade2
lixibi <116860773@qq.com>
2025-07-04 21:00:55 +0800

    bud

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CyGcHJAM.mjs';
-import { manifest } from './manifest_CB-dAQCV.mjs';
+import { manifest } from './manifest_D5ZYPwzE.mjs';
 
 const serverIslandMap = new Map([
 ]);;
@@ -8,44 +8,36 @@ const serverIslandMap = new Map([
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
 const _page2 = () => import('./pages/about.astro.mjs');
-const _page3 = () => import('./pages/admin.astro.mjs');
-const _page4 = () => import('./pages/admin-test.astro.mjs');
-const _page5 = () => import('./pages/categories/_category_.astro.mjs');
-const _page6 = () => import('./pages/categories.astro.mjs');
-const _page7 = () => import('./pages/notes/rss.xml.astro.mjs');
-const _page8 = () => import('./pages/notes/_---page_.astro.mjs');
-const _page9 = () => import('./pages/notes/_---slug_.astro.mjs');
-const _page10 = () => import('./pages/oauth/callback.astro.mjs');
-const _page11 = () => import('./pages/oauth.astro.mjs');
-const _page12 = () => import('./pages/og-image/_---slug_.png.astro.mjs');
-const _page13 = () => import('./pages/posts/_---page_.astro.mjs');
-const _page14 = () => import('./pages/posts/_---slug_.astro.mjs');
-const _page15 = () => import('./pages/recommendations.astro.mjs');
-const _page16 = () => import('./pages/rss.xml.astro.mjs');
-const _page17 = () => import('./pages/tags/_tag_/_---page_.astro.mjs');
-const _page18 = () => import('./pages/tags.astro.mjs');
-const _page19 = () => import('./pages/index.astro.mjs');
+const _page3 = () => import('./pages/categories/_category_.astro.mjs');
+const _page4 = () => import('./pages/categories.astro.mjs');
+const _page5 = () => import('./pages/notes/rss.xml.astro.mjs');
+const _page6 = () => import('./pages/notes/_---page_.astro.mjs');
+const _page7 = () => import('./pages/notes/_---slug_.astro.mjs');
+const _page8 = () => import('./pages/og-image/_---slug_.png.astro.mjs');
+const _page9 = () => import('./pages/posts/_---page_.astro.mjs');
+const _page10 = () => import('./pages/posts/_---slug_.astro.mjs');
+const _page11 = () => import('./pages/recommendations.astro.mjs');
+const _page12 = () => import('./pages/rss.xml.astro.mjs');
+const _page13 = () => import('./pages/tags/_tag_/_---page_.astro.mjs');
+const _page14 = () => import('./pages/tags.astro.mjs');
+const _page15 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["node_modules/.pnpm/astro@5.0.3_@types+node@22.10.1_jiti@1.21.6_rollup@4.28.0_typescript@5.7.2_yaml@2.6.1/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/404.astro", _page1],
     ["src/pages/about.astro", _page2],
-    ["node_modules/.pnpm/astro-decap-cms-oauth@0.5.1_astro@5.0.3_@types+node@22.10.1_jiti@1.21.6_rollup@4.28.0_typescript@5.7.2_yaml@2.6.1_/node_modules/astro-decap-cms-oauth/src/admin.astro", _page3],
-    ["src/pages/admin-test.astro", _page4],
-    ["src/pages/categories/[category].astro", _page5],
-    ["src/pages/categories/index.astro", _page6],
-    ["src/pages/notes/rss.xml.ts", _page7],
-    ["src/pages/notes/[...page].astro", _page8],
-    ["src/pages/notes/[...slug].astro", _page9],
-    ["node_modules/.pnpm/astro-decap-cms-oauth@0.5.1_astro@5.0.3_@types+node@22.10.1_jiti@1.21.6_rollup@4.28.0_typescript@5.7.2_yaml@2.6.1_/node_modules/astro-decap-cms-oauth/src/oauth/callback.ts", _page10],
-    ["node_modules/.pnpm/astro-decap-cms-oauth@0.5.1_astro@5.0.3_@types+node@22.10.1_jiti@1.21.6_rollup@4.28.0_typescript@5.7.2_yaml@2.6.1_/node_modules/astro-decap-cms-oauth/src/oauth/index.ts", _page11],
-    ["src/pages/og-image/[...slug].png.ts", _page12],
-    ["src/pages/posts/[...page].astro", _page13],
-    ["src/pages/posts/[...slug].astro", _page14],
-    ["src/pages/recommendations/index.astro", _page15],
-    ["src/pages/rss.xml.ts", _page16],
-    ["src/pages/tags/[tag]/[...page].astro", _page17],
-    ["src/pages/tags/index.astro", _page18],
-    ["src/pages/index.astro", _page19]
+    ["src/pages/categories/[category].astro", _page3],
+    ["src/pages/categories/index.astro", _page4],
+    ["src/pages/notes/rss.xml.ts", _page5],
+    ["src/pages/notes/[...page].astro", _page6],
+    ["src/pages/notes/[...slug].astro", _page7],
+    ["src/pages/og-image/[...slug].png.ts", _page8],
+    ["src/pages/posts/[...page].astro", _page9],
+    ["src/pages/posts/[...slug].astro", _page10],
+    ["src/pages/recommendations/index.astro", _page11],
+    ["src/pages/rss.xml.ts", _page12],
+    ["src/pages/tags/[tag]/[...page].astro", _page13],
+    ["src/pages/tags/index.astro", _page14],
+    ["src/pages/index.astro", _page15]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -55,7 +47,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "cd2c9bf3-190c-41ba-8362-b0c58a8b7acc",
+    "middlewareSecret": "7f877b67-193e-4df5-84bc-71af0eeacee1",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
